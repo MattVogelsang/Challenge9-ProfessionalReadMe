a0781ca4df337fe97328b8e0f6e960d5a9bf9a74
Mattvogelsang <vogelsangmatt@gmail.com>
2025-01-16 21:04:38 -0500

    saved

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 ## Description
 
-Write a readme
+The Professional README Generator is a command-line application designed to streamline the creation of high-quality, professional README files for GitHub repositories. By leveraging user input through the Inquirer package, this tool allows developers to generate well-structured README files quickly, saving time and ensuring consistency.
+
 
 <img src="img/Screenshot 2024-10-04 Homework Chally9.png">
 
@@ -28,13 +29,49 @@ Write a readme
 
 To install necessary dependencies, run the following command:
 
-```
-npm i
-```
+Clone the starter code from the repository.
+
+Navigate to the project directory in your terminal.
+
+Install the required dependencies by running the following command:
+
+npm i inquirer@8.2.4
+
+Create a .gitignore file and include the following to prevent unnecessary files from being tracked:
+node_modules/
+.DS_Store/
 
 ## Usage
 
-N/A
+To use the application, follow these steps:
+
+Open your terminal and navigate to the project directory.
+
+Run the application with the command:
+
+node index.js
+
+Follow the prompts to input details about your project, such as:
+
+Project title
+
+Description
+
+Installation instructions
+
+Usage information
+
+Contribution guidelines
+
+Test instructions
+
+License selection
+
+GitHub username
+
+Email address
+
+A professional README file will be generated in the project directory based on your input.
 
 ## License
 
@@ -42,7 +79,23 @@ This project is licensed under the MIT license.
   
 ## Contributing
 
-n/a
+Contributions are welcome! If you would like to contribute to this project:
+
+Fork the repository.
+
+Create a feature branch:
+
+git checkout -b feature/your-feature-name
+
+Commit your changes with descriptive messages:
+
+git commit -m "Add detailed description of changes"
+
+Push your changes to the branch:
+
+git push origin feature/your-feature-name
+
+Submit a pull request.
 
 ## Tests
 
@@ -57,3 +110,4 @@ npm test
 ## Questions
 
 If you have any questions about the repo, open an issue or contact me directly at vogelsangmatt@gmail.com You can find more of my work at [Mattvogelsang](https://github.com/MattVogelsang/).
+
--- index.js
+++ index.js
@@ -6,25 +6,8 @@ const inquirer=require("inquirer")
 const questions = [
     {
         type:"input",
-        message:"What is you're Github?",
-        name:"MattVogelsang",
-    }
-    ,
-    {
-        type:"input",
-        message:"What is you're Email?",
-        name:"Vogelsangmatt@gmail.com",
-    }
-    ,
-    {
-        type:"input",
-        message:"What is you're Project name?",
-        name:"ReadME Genertor",
-    }
-    , {
-        type:"input",
-        message:"Please write a short description of your project",
-        name:"Write a ReadME",
+        message:"What is the title of your project",
+        name:"title",
     }
     ,
     {
@@ -34,27 +17,41 @@ const questions = [
         choices:["MIT","ISC"]
     },
     {
-        type:"input",
-        message:"What Command should be run to install dependencies?",
-        name:"npm i",
-    }
-    ,
+        type: "input",
+        message: "Please provide a description of your project:",
+        name: "description",
+    },
     {
-        type:"input",
-        message:"what command should be run to test?",
-        name:"npm test",
-    }
-    
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What is the usage information?",
+        name: "usage",
+    },
+    {
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contributing",
+    },
+    {
+        type: "input",
+        message: "What are the test instructions?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
 ];
-// function renderLicenseLink(license) {
-//     const licenseLinks = {
-//       'MIT': '[MIT](https://opensource.org/licenses/MIT)',
-//       'GPLv3': '[GPLv3](https://www.gnu.org/licenses/gpl-3.0)',
-//       'Apache 2.0': '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)',
-//       'None': ''
-//     };
-//     return licenseLinks[license];
-//   }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
